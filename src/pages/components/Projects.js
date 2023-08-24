e2635e0e81182289f2f661f7dc62f3e3201a0d97
nolan nash <n0lan.n4sh@gmail.com>
2023-08-24 04:35:19 -0700

    test import issue fix

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import ProjectObjects from "./portfolio/projectObjects";
+import ProjectObjects from "./portfolio/ProjectObjects";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
     const [isHovered, setIsHovered] = useState(false);
     const isPTagVisible = isHovered;
     const projectCardsContainerMarginTop = isPTagVisible ? "mt-8 md:mt-6 lg:mt-4" : "mt-0 md:mt-2 lg:mt-1";
-
+    const projects = ProjectObjects
     return (
         <div
             className={`relative top-0 left-0 h-full items-center bg-transparent rounded-3xl outline-double outline-8 outline-sky-700 border-4 border-sky-700 shadow-2xl shadow-pink-400/30 ${projectCardsContainerMarginTop}`}
@@ -32,7 +32,7 @@ const Projects = () => {
                 className="overflow-y overflow-auto shadow-2xl rounded-3xl p-2"
                 style={{ maxHeight: 'calc(100vh - 350px)' }}
             >
-                {ProjectObjects.map(project => (
+                {projects.map(project => (
                     <div
                         key={project.name}
                         className="transition-transform duration-300 transform hover:scale-105 hover:shadow-xl shadow-rounded hover:bg-opacity-100"
