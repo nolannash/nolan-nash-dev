c4ee0c29a5315a71459d65355f1465a67710d38e
nolan nash <n0lan.n4sh@gmail.com>
2023-08-24 04:46:15 -0700

    test 3

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-
 import ProjectCard from "./ProjectCard";
 
-const ProjectObjects = [
+const ProjectArr = [
     {
         name: 'Note Nest',
         technologies: ['Javascript', 'MongoDB', 'Next.js', 'Tailwind CSS', 'Google Authentication', 'bcrypt.js', 'Router'],
@@ -50,34 +49,23 @@ const ProjectObjects = [
 
 const Projects = () => {
     const [isHovered, setIsHovered] = useState(false);
-    const isPTagVisible = isHovered;
-    const projectCardsContainerMarginTop = isPTagVisible ? "mt-8 md:mt-6 lg:mt-4" : "mt-0 md:mt-2 lg:mt-1";
-    const projects = ProjectObjects
+    const projects = ProjectArr;
+
     return (
-        <div
-            className={`relative top-0 left-0 h-full items-center bg-transparent rounded-3xl outline-double outline-8 outline-sky-700 border-4 border-sky-700 shadow-2xl shadow-pink-400/30 ${projectCardsContainerMarginTop}`}
-            style={{ fontFamily: "'VT323', sans-serif" }}
-        >
-            <h1
-                className={`text-6xl font-bold text-center text-white ${
-                    isHovered ? "glow-effect" : ""
-                } transition-all transition-all`}
+        <div className={`relative top-0 left-0 h-full items-center bg-transparent rounded-3xl outline-double outline-8 outline-sky-700 border-4 border-sky-700 shadow-2xl shadow-pink-400/30 mt-0 md:mt-2 lg:mt-1`} style={{ fontFamily: "'VT323', sans-serif" }}>
+            <div
+                className={`text-6xl font-bold text-center text-white glow-effect transition-all transition-all`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                Portfolio
-            </h1>
-            <p
-                className={`text-center ${
-                    isHovered ? "visible opacity-100 mb-4 text-xl" : "invisible opacity-0 mt-0"
-                } transition-opacity duration-300`}
-            >
-                Hi there and welcome to my portfolio. Please feel free to take a look at my projects. I am always looking to start new projects.
-            </p>
-            <div
-                className="overflow-y overflow-auto shadow-2xl rounded-3xl p-2"
-                style={{ maxHeight: 'calc(100vh - 350px)' }}
-            >
+                <h1>Portfolio</h1>
+            </div>
+            {isHovered && (
+                <div className={`text-center visible opacity-100 mb-4 text-xl transition-opacity duration-300`}>
+                    <p>Hi there and welcome to my portfolio. Please feel free to take a look at my projects. I am always looking to start new projects.</p>
+                </div>
+            )}
+            <div className="overflow-y overflow-auto shadow-2xl rounded-3xl p-2" style={{ maxHeight: 'calc(100vh - 350px)' }}>
                 {projects.map(project => (
                     <div
                         key={project.name}
